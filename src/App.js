4579dcefb9ea8ffc5fda40aca52536b0005b79aa
yodifadhilabdhusshabur <80160731+yodifadhilabdhusshabur@users.noreply.github.com>
2021-03-09 16:44:49 +0700

    dev-yodi-version1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import LoginButton from "./LoginButton";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <LoginButton/>
+            </div>
+        );
+    }
+}
+
+export default App;
